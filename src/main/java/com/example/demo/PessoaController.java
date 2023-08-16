d66be5f754d41d15f8503c872d3c723dd68c1f5c
Pedro Alonso <phaalonso@gmail.com>
2023-08-15 23:54:27 -0300

    Primeira versão que funcionou no gatling

--- src/main/java/com/example/demo/PessoaController.java
+++ src/main/java/com/example/demo/PessoaController.java
@@ -1,24 +1,17 @@
 package com.example.demo;
 
-import akka.pattern.StatusReply;
-import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/people")
 public class PessoaController {
-//    @GetMapping("health")
-//    public String getHealth() {
-//        return "Ok";
-//    }
     private final PessoaRepository pessoaRepository;
 
     @Autowired
@@ -26,7 +19,14 @@ public class PessoaController {
         this.pessoaRepository = pessoaRepository;
     }
 
-    @PostMapping
+    @GetMapping("/contagem-pessoas")
+    public ResponseEntity<Integer> contagemPessoa() {
+        var result = this.pessoaRepository.count();
+
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/pessoas")
     @Transactional
     public ResponseEntity createPeople(@RequestBody PessoaModel model) throws SQLException {
         if (model.getNick().length() > 32) {
@@ -45,10 +45,18 @@ public class PessoaController {
 
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/pessoas")
+    public ResponseEntity<List<PessoaModel>> findBessoa(@RequestParam String t) {
+        var result = this.pessoaRepository.findByTermo(t);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/pessoas/{id}")
     public ResponseEntity<PessoaModel> getPessoaById(@PathVariable String id) {
         var result = this.pessoaRepository.findById(id);
 
         return ResponseEntity.ok(result);
     }
+
 }
