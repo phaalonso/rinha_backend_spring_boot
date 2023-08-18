d41a084ebe103e185ba3cb86a86c8aba4017646b
Pedro Alonso <phaalonso@gmail.com>
2023-08-18 00:23:37 -0300

    Toggle virtual threads

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM eclipse-temurin:17-jdk-alpine
+FROM eclipse-temurin:19-jdk-alpine
 VOLUME /tmp
 COPY run.sh .
 COPY target/*.jar app.jar
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +18,21 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+	@Bean
+	@ConditionalOnProperty(value = "virtualThreads.enabled", havingValue = "true")
+	AsyncTaskExecutor applicationTaskExecutor() {
+		System.out.println("Ativando virtual threads");
+		// enable async servlet support
+		ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+		return new TaskExecutorAdapter(executorService);
+	}
+
+	@Bean
+	@ConditionalOnProperty(value = "virtualThreads.enabled", havingValue = "true")
+	TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+		return protocolHandler -> {
+			protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+		};
+	}
 }
